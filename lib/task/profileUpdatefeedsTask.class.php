cfbe87cd7baa4ead5e51366c9000a62c9151b4d1
Isofarro <isofarro2@gmail.com>
2009-04-10 09:19:57 +0100

    created update-feeds symfony task

---
+++ lib/task/profileUpdatefeedsTask.class.php
@@ -0,0 +1,38 @@
+<?php
+
+class profileUpdatefeedsTask extends sfPropelBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'propel'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'profile';
+    $this->name             = 'update-feeds';
+    $this->briefDescription = 'Updates profile feeds';
+    $this->detailedDescription = <<<EOF
+The [profile:update-feeds|INFO] task updates all the feeds.
+Call it with:
+
+  [php symfony profile:update-feeds|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'] ? $options['connection'] : null)->getConnection();
+
+    // add your code here
+  }
+}
