76e0a76eb58beb5793c6b069f6960ba0f1c7373c
Isofarro <isofarro2@gmail.com>
2009-04-11 15:57:42 +0100

    Comment the Peer methods

--- lib/model/ItemPeer.php
+++ lib/model/ItemPeer.php
@@ -2,10 +2,17 @@
 
 class ItemPeer extends BaseItemPeer {
 
+	/**
+	* Gets all the items for a given site
+	* @param $site_id - the id of the site
+	* @return $items - an array of items for that site
+	**/
 	public static function getSiteFeedItems($site_id) {
 		$itemCriteria = new Criteria();
 		
-		// TODO: filter to items belonging to a site feed
+		// Get items belonging to a site feed
+		$itemCriteria->addJoin(self::FEED_ID, SitefeedPeer::FEED_ID);
+		$itemCriteria->add(SitefeedPeer::SITE_ID,	$site_id, Criteria::IN);
 
 		// order by published date Descending
 		$itemCriteria->addDescendingOrderByColumn(self::PUBLISHED);
@@ -15,20 +22,21 @@ class ItemPeer extends BaseItemPeer {
 	}
 	
 	/**
-	 * Gets all the items for a site broken down into collections
+	 * Gets all the items for a site separated into collections
 	 * @param $site_id - the id of the site
 	 * @return collection - a collection hash, keyed by collection name
 	 *			each value is an array of items 
 	 **/
 	public static function getCollectedFeedItems($site_id) {
-		// Get the collections map, keyed by feed id
+		// Get the feed collections map for a site
 		$collections = SitefeedPeer::getSiteFeedCollections($site_id);
 		
 		// Get all the items for the site
-		$items       = ItemPeer::getSiteFeedItems($site_id);
+		$items       = self::getSiteFeedItems($site_id);
 		
 		$sortedItems = array();		
 		
+		// Sort the items into their categories
 		foreach($items as $item) {
 			$feedid = $item->getFeedId();
 			$collection = $collections[$feedid];
--- lib/model/SitefeedPeer.php
+++ lib/model/SitefeedPeer.php
@@ -2,19 +2,24 @@
 
 class SitefeedPeer extends BaseSitefeedPeer {
 
+	/**
+	* Get all the feeds for the specified site
+	* @param $site_id - the id of the site
+	* @return $feeds - an array of feeds that makes up the site
+	**/
 	public static function getSiteFeeds($site_id) {
 		$sitefeedCriteria = new Criteria();
-		$sitefeedCriteria->add(
-			SitefeedPeer::SITE_ID,
-			$site_id,
-			Criteria::EQUAL
-		);
-	
-		return SitefeedPeer::doSelect($sitefeedCriteria);
+		$sitefeedCriteria->add(self::SITE_ID, $site_id, Criteria::EQUAL);
+		return self::doSelect($sitefeedCriteria);
 	}
 	
+	/**
+	* Get a hash of site collections, keyed by feed id
+	* @param $site_id - the id of the site
+	* @return $collections - a feed collection map, keyed by feed id.
+	**/
 	public static function getSiteFeedCollections($site_id) {
-		$feeds = SitefeedPeer::getSiteFeeds($site_id);
+		$feeds = self::getSiteFeeds($site_id);
 		
 		$collections = array();
 		foreach($feeds as $feed) {
