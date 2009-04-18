276aeaacccbf88cda2557d6bed3319aed81233cc
Isofarro <isofarro2@gmail.com>
2009-04-18 11:28:20 +0100

    Set the default site in the front controller

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -10,4 +10,5 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
+sfConfig::add(array('app_default_site' => 1));
 sfContext::createInstance($configuration)->dispatch();
